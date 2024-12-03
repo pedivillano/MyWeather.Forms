088a8bcea4ddfd3e8df4f585556f9bf1740fe76e
Pedi <75835901+pedivillano@users.noreply.github.com>
2024-12-03 09:59:45 +0800

    Update appcenter-post-clone.sh

--- MyWeather.Droid/appcenter-post-clone.sh
+++ MyWeather.Droid/appcenter-post-clone.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-echo "123"
+echo "huimeng123"
+echo $APPCENTER_SOURCE_DIRECTORY
+SLN_FILE=$APPCENTER_SOURCE_DIRECTORY/MyWeather.Forms/MyWeather.sln
+ANDROID_CSPROJ_FILE=$APPCENTER_SOURCE_DIRECTORY/MyWeather.Forms/MyWeather.iOS/MyWeather.iOS.csproj
+dotnet sln $SLN_FILE remove $ANDROID_CSPROJ_FILE
