e2ee8c2610ff61568a69fcc90ede0bfcc402fcf7
Pedi <75835901+pedivillano@users.noreply.github.com>
2024-12-03 10:37:25 +0800

    Update appcenter-post-clone.sh

--- MyWeather.Droid/appcenter-post-clone.sh
+++ MyWeather.Droid/appcenter-post-clone.sh
@@ -7,3 +7,20 @@ WINDOWS_CSPROJ_FILE=$APPCENTER_SOURCE_DIRECTORY/MyWeather.UWP/MyWeather.UWP.cspr
 dotnet sln $SLN_FILE remove $IOS_CSPROJ_FILE
 dotnet sln $SLN_FILE remove $WINDOWS_CSPROJ_FILE
 
+export PATH=$PATH:$HOME/Library/Android/sdk/cmdline-tools/latest/bin
+
+ 
+
+echo "Installing missing SDK platforms"
+
+ 
+
+sdkmanager "platforms;android-27" "platforms;android-28" "platforms;android-29" "platforms;android-30"
+
+ 
+
+echo "Missing SDK platforms installed. Installing missing build tools."
+
+ 
+
+sdkmanager "build-tools;27.0.0" "build-tools;27.0.1" "build-tools;27.0.2" "build-tools;27.0.3" "build-tools;28.0.0" "build-tools;28.0.1" "build-tools;28.0.2" "build-tools;28.0.3" "build-tools;29.0.0" "build-tools;29.0.1" "build-tools;29.0.2" "build-tools;29.0.3" "build-tools;30.0.0" "build-tools;30.0.1" "build-tools;30.0.2" "build-tools;30.0.3"
