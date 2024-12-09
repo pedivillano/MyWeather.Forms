a073457a3780d46928c69ac164db6a3077c33462
Pedi <75835901+pedivillano@users.noreply.github.com>
2024-12-09 12:28:51 +0800

    Update appcenter-post-clone.sh

--- MyWeather.Droid/appcenter-post-clone.sh
+++ MyWeather.Droid/appcenter-post-clone.sh
@@ -37,22 +37,16 @@ for p in "$IOS_PATHS"; do
 done
 fi
 fi
- 
+
 
 export PATH=$PATH:$HOME/Library/Android/sdk/cmdline-tools/latest/bin
 
- 
+
 
 echo "Installing missing SDK platforms"
 
- 
-
 sdkmanager "platforms;android-27" "platforms;android-28" "platforms;android-29" "platforms;android-30"
 
- 
-
 echo "Missing SDK platforms installed. Installing missing build tools."
-
- 
 
 sdkmanager "build-tools;27.0.0" "build-tools;27.0.1" "build-tools;27.0.2" "build-tools;27.0.3" "build-tools;28.0.0" "build-tools;28.0.1" "build-tools;28.0.2" "build-tools;28.0.3" "build-tools;29.0.0" "build-tools;29.0.1" "build-tools;29.0.2" "build-tools;29.0.3" "build-tools;30.0.0" "build-tools;30.0.1" "build-tools;30.0.2" "build-tools;30.0.3"
